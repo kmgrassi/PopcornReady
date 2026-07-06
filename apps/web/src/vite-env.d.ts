f19a9f5be1767800740bd68a4078f1b2bd991647
kmgrassi <kmgrassi@gmail.com>
2026-07-06 10:39:23 -0400

    Tighten web generation-run typing (#655)
    * Tighten web generation run typing
    
    * Accept runId in generation run client guard

---
+++ apps/web/src/vite-env.d.ts
@@ -0,0 +1,14 @@
+interface ImportMetaEnv {
+  readonly VITE_API_URL?: string;
+  readonly VITE_SUPABASE_ENV?: string;
+  readonly VITE_SUPABASE_URL?: string;
+  readonly VITE_SUPABASE_ANON_KEY?: string;
+  readonly VITE_SUPABASE_DEV_URL?: string;
+  readonly VITE_SUPABASE_DEV_ANON_KEY?: string;
+  readonly VITE_SUPABASE_PROD_URL?: string;
+  readonly VITE_SUPABASE_PROD_ANON_KEY?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
